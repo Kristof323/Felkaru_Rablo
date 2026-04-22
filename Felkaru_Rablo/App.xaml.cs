1f20d21ab7de31ae94149df7c3ac4859c37f6515
Kristof323 <kristofcsapo@gmail.com>
2026-04-22 09:33:19 +0200

    Add project files.

---
+++ Felkaru_Rablo/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Felkaru_Rabló
+{
+    public partial class App : Application
+    {
+    }
+}
